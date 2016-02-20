78c4db1db841bc71558778b8cf351f5fd4f77c66
gregory nicholas <gregory@unvael.com>
2016-02-20 04:03:20 -0800

    Update package.js

--- package.js
+++ package.js
@@ -1,19 +1,18 @@
 Package.describe({
-  name: 'fongandrew:find-and-modify',
-  summary: 'findAndModify implementation for Meteor collection',
-  version: '0.2.1',
-  git: 'https://github.com/fongandrew/meteor-find-and-modify.git'
+  name: 'gregorynicholas:find-and-modify',
+  summary: 'meteor findAndModify (server only)',
+  version: '0.2.1.1',
+  git: 'https://github.com/gregorynicholas/meteor-find-and-modify.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.4');
-  api.use('mongo', ['client', 'server']);
-  api.imply('mongo', ['client', 'server'])
-  api.addFiles('find_and_modify.js', ['client', 'server']);
+  api.versionsFrom('1.2.1');
+  api.use('mongo', ['server']);
+  api.addFiles('find-and-modify.js', ['server']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('fongandrew:find-and-modify');
+  api.use('gregorynicholas:find-and-modify');
   api.addFiles('find_and_modify_tests.js');
 });
